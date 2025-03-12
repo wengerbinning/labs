61cb52596c2551f8078a3a30caacbf71a691106c
Wenbin Zheng <wbzheng@grandstream.cn>
2025-03-12 10:20:42 +0800

    2025-03-12 00

--- c/curl/build.sh
+++ c/curl/build.sh
@@ -2,15 +2,6 @@
 
 
 
-
-
-
-./configure --host=$TARGET \
-    --prefix=/usr \
-    UCI_CFLAGS=$TOOLCHAIN_PATH/usr/include UCI_LIBS=$TOOLCHAIN_PATH/usr/lib/libuci.so
-
-
-
-
-
+test -f configure || ./bootstrap
+./configure
 make
