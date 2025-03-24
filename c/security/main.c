24c906da2cbcd6e785f7336f48c847f9bcee74d5
Wenbin Zheng <wbzheng@grandstream.cn>
2025-03-24 19:09:12 +0800

    2025-03-24 01

---
+++ c/security/main.c
@@ -0,0 +1,98 @@
+
+
+
+
+/*
+checksum
+
+
+
+
+
+Hash: A hash function is any function that can be used to map data of arbitrary
+size to fixed-size values, though there are some hash functions that support
+variable-length output.
+
+Hash tables:The values returned by a hash function are called hash values, hash
+codes, (hash/message) digests, or simply hashes. The values are usually used to
+index a fixed-size table called a hash table.
+
+- Fibonacci hashing
+
+
+
+
+MD5
+
+SHA-256
+
+
+* Verifying the integrity of messages and files
+* Signature generation and verification
+* Password verification
+
+
+
+
+<https://en.wikipedia.org/wiki/Merkle%E2%80%93Damg%C3%A5rd_construction>
+
+
+
+*
+* 消息块
+* 处理块
+* 摘要
+
+
+* MD4:      消息块长度: 512比特, 处理块长度:32比特, 摘要长度:128比特
+* MD5:      消息块长度: 512比特, 处理块长度:32比特, 摘要长度:128比特
+* SHA1:     消息块长度: 512比特, 处理块长度:32比特, 摘要长度:160比特
+* SHA2-224: 消息块长度: 512比特, 处理块长度:32比特, 摘要长度:224比特
+* SHA2-256: 消息块长度: 512比特, 处理块长度:32比特, 摘要长度:256比特
+* SHA2-384: 消息块长度:1024比特, 处理块长度:32比特, 摘要长度:384比特
+* SHA2-512: 消息块长度:1024比特, 处理块长度:32比特, 摘要长度:512比特
+* SHA3-224: 消息块长度:1152比特, 处理块长度:32比特, 摘要长度:224比特
+* SHA3-256: 消息块长度:1088比特, 处理块长度:32比特, 摘要长度:256比特
+* SHA3-384: 消息块长度: 832比特, 处理块长度:32比特, 摘要长度:384比特
+* SHA3-512: 消息块长度: 576比特, 处理块长度:32比特, 摘要长度:512比特
+
+ */
+
+
+
+/*
+MAC
+
+* DAA
+* CBC-MAC
+* HMAC
+* OMAC/CMAC
+* PMAC
+* VMAC
+* UMAC
+* Poly1305
+ */
+/*
+HMAC(Hash-based Message Authentication Code)
+---------------------------------------------
+基于哈希的消息认证码 - 是一种使用哈希与加密实现的加密认证技术
+
+
+
+
+- HMAC(MD5)
+- HMAC(SHA1)
+- HMAC(SHA-256)
+- HMAC(MD5)
+
+
+
+
+
+
+
+ */
+int main(int argc, char *agrv[]) {
+
+	return 0;
+}
