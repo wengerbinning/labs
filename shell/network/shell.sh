c7949def709bcc3b0d2505c492111db747be3e28
Wenbin Zheng <wbzheng@grandstream.cn>
2025-06-19 14:56:29 +0800

    [shell] add api

--- shell/network/shell.sh
+++ shell/network/shell.sh
@@ -2,6 +2,8 @@ CMD_SHOW=false
 CMD_ROOT=true
 
 
+# 将子网掩码转化为掩码
+echo 255.255.255.0 | awk -F. -e 'BEGIN {n=0} {for(r=1;r<=NF;r++)for(i=0;i<8;i++){if(and(lshift($r,i),0x80)==0)exit 0; n+=1}} END {print n}'
 
 
 runcmd $@
