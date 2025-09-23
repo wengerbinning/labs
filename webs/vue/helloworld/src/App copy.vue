2a21339cd5b360d6b2da6619c1abcd8e8090cfc5
Wenbin Zheng <wbzheng@grandstream.cn>
2025-09-23 16:25:44 +0800

    demo

--- webs/vue/helloworld/src/App copy.vue
+++ webs/vue/helloworld/src/App copy.vue
@@ -5,9 +5,17 @@ import CallGraph  from './components/CallGraph.vue'
 </script>
 
 <template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <CallGraph />
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
 
+  <main>
+    <TheWelcome />
+  </main>
 </template>
 
 <style scoped>
---
+++ webs/vue/helloworld/src/components/CallGraph.vue
@@ -0,0 +1,78 @@
+<script setup>
+
+</script>
+
+
+
+<template>
+    <div>
+        <ul>
+            <li v-for="(each, idex) in type" :key="each.id">
+                {{ each.id }} - {{ each.name }}
+            </li>
+        </ul>
+        <p>内容</p>
+        <ul>
+            <li v-for="(each, idex) in data" :key="each.name">
+                {{ each.name }} - {{ each.type }} {{ each.attrs }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            type: [
+                { id: "type",     name: "数据类型" },
+                { id: "object",   name: "数据对象" },
+                { id: "function", name: "函数接口" }
+            ],
+            data: [
+                {
+                    name: "int",
+                    type: "type",
+                    size: 4,
+                    opts: [],
+                    apis: [],
+
+                    attrs: ["standard"],
+                    scope: [],
+                    label: [],
+                    state: [],
+                },
+                {
+                    name: "var",
+                    type: "object",
+                    size: 1,
+                    opts: [],
+                    apis: [],
+
+                    attrs: ["standard"],
+                    scope: [],
+                    label: [],
+                    state: [],
+                },
+                {
+                    name: "func",
+                    type: "function",
+                    size: 1,
+                    opts: [],
+                    apis: [],
+
+                    attrs: ["standard"],
+                    scope: [],
+                    label: [],
+                    state: [],
+
+                    params: [],
+                    return: []
+
+                }
+            ]
+        }
+    }
+}
+</script>
