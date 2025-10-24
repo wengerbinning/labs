34efbb99e4ed73199927ae8909381e47b74d5a05
Wenbin Zheng <wbzheng@grandstream.cn>
2025-10-24 19:05:22 +0800

    de

---
+++ c/Untitled-1.c
@@ -0,0 +1,37 @@
+
+
+
+
+
+struct demo {
+	float cur_
+	float avg_
+
+};
+
+
+/*
+ 委托价格
+ 成交价格
+ 成交数量
+
+ */
+
+struct test {
+	float price;
+	int   count;
+
+
+};
+
+
+
+/*
+
+ - 监控流量
+ - 流量统计(累计, 速率)
+ - 流量过滤
+ - 流量分析
+ - 流量识别
+
+ */
