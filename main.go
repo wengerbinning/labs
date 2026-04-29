154805fd5e210190b31fa8f135f558e41ba51513
Wenbin Zheng <wbzheng@grandstream.cn>
2026-04-29 16:07:39 +0800

    2026-04-29 03

--- main.go
+++ main.go
@@ -1,24 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"encoding/json"
 )
 
 
 
 type DevelHandler struct{}
 func (h DevelHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"message": "Hello, World!"})
 }
 
 
 
-
+func devel(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("%s %s\n", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("/", DevelHandler{})
 
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":8080", devel(mux))
 }
