be4b9e7193914db49b827c0fab046ec417b976ce
Wenbin Zheng <wbzheng@grandstream.cn>
2026-04-29 15:52:10 +0800

    2026-04-29 01

--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"encoding/json"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/" , func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	})
 
